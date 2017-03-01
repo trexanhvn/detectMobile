85403061716e91c4c3537ab5551fbb22e1a8bfde
TreXanh VN <trexanhvn.ddthvn@gmail.com>
2017-03-01 16:45:31 +0700

    Change function to get info devices

--- getInfo.js
+++ getInfo.js
@@ -1,31 +1,34 @@
-function parse () {
+function running(json) {
+    var client = new ClientJS();
+    var infoDevice = {
+        browser: client.getBrowser(),
+        OS: client.getOS(),
+        OSVersion: client.getOSVersion(),
+        screenResolution: client.getCurrentResolution(),
+    }
+    var yes = document.getElementsByClassName('question-yes')[0];
+    yes.addEventListener("click", function (event) {
+        parse(json.ip, event.target, infoDevice);
+    });
+    var no = document.getElementsByClassName('question-no')[0];
+    no.addEventListener("click", function (event) {
+        parse(json.ip, event.target, infoDevice)
+    });
+}
+
+function parse(ipAddress, ele, infoDevice) {
     Parse.initialize("ButterflyWow", "");
     Parse.serverURL = "http://ec2-54-214-97-245.us-west-2.compute.amazonaws.com:1337/parse";
-
     var TestObject = Parse.Object.extend("TestObject");
     var testObject = new TestObject();
-    testObject.save({clientIP: getIP()}).then(function(object) {
-        alert("yay! it worked");
+    testObject.save({
+        idStore: location.search.substr(1).split('id=')[1],
+        clientIP: ipAddress,
+        answer: ele.getAttribute('val'),
+        info: infoDevice
+    }).then(function(object) {
+        console.log(document.getElementsByClassName('question-section'))
+        document.getElementsByClassName('question-section')[0].style.display = 'none'
+        document.getElementsByClassName('alert')[0].style.display = 'block'
     })
-}
-function getIP(json) {
-    axios.get('https://api.ipify.org?format=jsonp').then(function (json){
-        console.log(testing(json))
-    })
-
-}
-function testing(data) {
-    document.write("My public IP address is: ", data.ip);
-}
-function handleAction () {
-    document.getElementsByClassName('btn-success').addEventListener("click", handleYes());
-    document.getElementsByClassName('btn-primary').addEventListener("click", handleNo());
-}
-
-function handleYes() {
-    console.log('Yes');
-}
-
-function handleNo() {
-    console.log('No');
 }
